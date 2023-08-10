23c3dbbb4d2d5641029a2f3083264ac2ed6cfde9
Yang-Seung-Ho <79961121+Yang-Seung-Ho@users.noreply.github.com>
2023-08-10 16:42:11 +0900

    Add files via upload

--- res/ship/ncommon.py
+++ res/ship/ncommon.py
@@ -29,31 +29,41 @@ def y_nateLogin(NateID, NatePD) :
     global nateDriver, wait_nate
 
 
-    # 네이트 접속
-    nateDriver = webdriver.Chrome()
-    nateDriver.maximize_window()
-    nateDriver.get("https://www.nate.com/")
-    wait_nate = WebDriverWait(nateDriver, 10)  # 최대 10초까지 기다립니다.
+    # 네이트 접속 및 로그인 오류 시 창 닫기 후 반복
+    while True :
+          try :
+                # 네이트 접속
+            nateDriver = webdriver.Chrome()
+            nateDriver.maximize_window()
+            nateDriver.get("https://www.nate.com/")
+            wait_nate = WebDriverWait(nateDriver, 10)  # 최대 10초까지 기다립니다.
 
 
-    # 네이트 아이디 입력
-    IdInputBox = wait_nate.until(EC.presence_of_element_located(
-        (By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/form/fieldset/input[1]")))
-    IdInputBox.send_keys(NateID)  
+            # 네이트 아이디 입력
+            IdInputBox = wait_nate.until(EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/form/fieldset/input[1]")))
+            IdInputBox.send_keys(NateID)  
 
 
-    # 네이트 비밀번호 입력
-    PassWordInputBox = wait_nate.until(EC.presence_of_element_located(
-        (By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/form/fieldset/input[2]")))
-    PassWordInputBox.send_keys(NatePD)  
-    time.sleep(1)
+            # 네이트 비밀번호 입력
+            PassWordInputBox = wait_nate.until(EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/form/fieldset/input[2]")))
+            PassWordInputBox.send_keys(NatePD)  
+            time.sleep(1)
 
 
-    # 네이트 로그인 버튼 클릭
-    ToLoginBtn = wait_nate.until(EC.presence_of_element_located(
-        (By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/form/fieldset/input[4]")))
-    ToLoginBtn.click()
-    time.sleep(1)
+            # 네이트 로그인 버튼 클릭
+            ToLoginBtn = wait_nate.until(EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div[1]/div[2]/div[2]/div[1]/form/fieldset/input[4]")))
+            ToLoginBtn.click()
+            time.sleep(1)
+            break
+          except:
+            time.sleep(1)
+            nateDriver.close()
+            time.sleep(1)
+            continue
+    
 
 
 
@@ -132,55 +142,66 @@ def y_nateEmailMsg():
 # 네이트 메일 보내기 접속, 메일 전송
 def y_nateEmailSend(SendEmail1, SendEmail2):
     EmailSubject, EmailMsg = y_nateEmailMsg()
-    try :
-        # 메일 보내기 접속
-        nateDriver.get("https://mail3.nate.com/#write/?act=new")
-        time.sleep(3)
-        # 상대 이메일 주소 입력
-        EmailAddressBox = wait_nate.until(EC.presence_of_element_located(
-            (By.ID, "textArea__to")))
-        EmailAddressBox.clear()  # 기존 값 삭제
-        EmailAddressBox.send_keys(SendEmail1)
-        EmailAddressBox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        EmailAddressBox.send_keys(SendEmail2)
-        EmailAddressBox.send_keys(Keys.ENTER)
+    while True :
+        try :
+            # 메일 보내기 접속
+            nateDriver.get("https://mail3.nate.com/#write/?act=new")
+            time.sleep(3)
+            # 상대 이메일 주소 입력
+            EmailAddressBox = wait_nate.until(EC.presence_of_element_located(
+                (By.ID, "textArea__to")))
+            EmailAddressBox.clear()  # 기존 값 삭제
+            EmailAddressBox.send_keys(SendEmail1)
+            EmailAddressBox.send_keys(Keys.ENTER)
+            time.sleep(1)
+            EmailAddressBox.send_keys(SendEmail2)
+            EmailAddressBox.send_keys(Keys.ENTER)
 
 
-        # 제목 입력
-        MailSubjectBox = wait_nate.until(EC.presence_of_element_located(
-            (By.ID, "mail_subject")))
-        MailSubjectBox.send_keys(EmailSubject)
+            # 제목 입력
+            MailSubjectBox = wait_nate.until(EC.presence_of_element_located(
+                (By.ID, "mail_subject")))
+            MailSubjectBox.send_keys(EmailSubject)
 
 
-        # iframe 전환
-        iframe = nateDriver.find_element(By.ID, "editor_iframe")
-        nateDriver.switch_to.frame(iframe)
+            # iframe 전환
+            iframe = nateDriver.find_element(By.ID, "editor_iframe")
+            nateDriver.switch_to.frame(iframe)
 
 
-        # 예약요청 입력 시작
-        InfoBox = nateDriver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[3]/div[2]")
-        InfoBox.clear()
-        InfoBox.send_keys(Keys.CONTROL, 'a')  # 텍스트 전체 선택
+            # 예약요청 입력 시작
+            InfoBox = nateDriver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[3]/div[3]")
+            InfoBox.clear()
+            InfoBox.send_keys(Keys.CONTROL, 'a')  # 텍스트 전체 선택
 
 
-        # 예약 메시지 입력 후 iframe 기본 전환
-        InfoBox.send_keys(EmailMsg)
-        time.sleep(1)
-        nateDriver.switch_to.default_content()
-        EmailAddressBox.click()
+            # 예약 메시지 입력 후 iframe 기본 전환
+            InfoBox.send_keys(EmailMsg)
+            time.sleep(1)
+            nateDriver.switch_to.default_content()
+            EmailAddressBox.click()
+            
+            
+            # 메일 검토
+            answer = input("메일 검토 _메일 전송 a, 재시도 b, 종료할거면 c")
+            if  answer == 'a':
+
+
+                # 메일 보내기 버튼 클릭
+                sendButton = nateDriver.find_element(By.XPATH, '/html/body/div[11]/div[4]/div/div/div[1]/div[2]/div[5]/div[2]/div/div[1]/div[1]/div[1]/button')
+                sendButton.click()
+                time.sleep(3)
+                break        
+            elif answer == 'b' :
+                continue
+            
+            elif answer == 'c' :
+                quit()
+        except :
+            print("네이트 메일 보내던 중 오류")
+            common.printErrMsg()
+            time.sleep(3)
+            
         
-        
-        # 메일 검토
-        common.gostop_quit("메일 보낼거면 a, 아니면 아무거나")
-        # 메일 보내기 버튼 클릭
-        sendButton = nateDriver.find_element(By.XPATH, '/html/body/div[11]/div[4]/div/div/div[1]/div[2]/div[5]/div[2]/div/div[1]/div[1]/div[1]/button')
-        sendButton.click()
-        time.sleep(3)
-        
-    except :
-        print("네이트 메일 보내던 중 오류")
-        common.printErrMsg()
-        time.sleep(3)
         
         
